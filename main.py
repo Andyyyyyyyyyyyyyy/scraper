02d1bc53d8776809385a139f089c3db3a3d42214
haroon01 <haroon815@gmail.com>
2020-11-05 22:15:35 +0000

    first version completed

--- main.py
+++ main.py
@@ -32,24 +32,29 @@ def load_db(): # this will load domains.csv into domain_list
         for row in reader:
             str = "".join(row)
             domain_list.append(str)
-        print(domain_list)
 
 def scan_submissions():
-    print("scan submissions")
-    for submission in reddit.subreddit(subreddit).new(limit=10):
-        if not submission.is_self:
-            link = submission.url
-            link_domain = extract_domain(link)[0]
-            for domain in domain_list:
-                if domain == link_domain:
-                    print(f"domain match found {domain}")
-                    title = submission.title
-                    domain_name = link_domain.split(".")[0]
-                    headline = getattr(scraper, domain_name)(link) # INFO: call a function using a variable name "getattr(module, varname)(args)"
-                    print(headline)
-                    # TODO: compare submission titles to headline and do the most appropriate action
-
-
+    try:
+        for submission in reddit.subreddit(subreddit).stream.submissions(skip_existing=True):
+            if not submission.is_self:
+                link = submission.url
+                link_domain = extract_domain(link)[0]
+                for domain in domain_list:
+                    if domain == link_domain:
+                        title = submission.title
+                        domain_name = link_domain.split(".")[0]
+                        headline = getattr(scraper, domain_name)(link) # call a function using a variable name "getattr(module, varname)(args)"
+                        # TODO: compare submission titles to headline and do the most appropriate action
+                        if headline != title:
+                            comment = submission.reply("Hey, your submission has been removed! \n\n "
+                                             "Reason: \n\n "
+                                             "```Submission Title does not match the one in the article you "
+                                                   "linked to!``` \n\n Please repost your submission and be sure to make the title "
+                                                   "the **exact same** as the headline in the article! Thank you! \n\n ^^I ^^am ^^a ^^bot")
+                            comment.mod.distinguish(how="yes", sticky=True)
+                            submission.mod.remove(spam=False, mod_note="Title did not match headline.")
+    except Exception as e:
+        print(f"** ERROR: {e}")
 
 def initialise():
     load_db()
